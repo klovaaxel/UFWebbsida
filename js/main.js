69e69507b34ad54d881ff320d6bc852137fb8c5a
klovaaxel <klovakarlsson@gmail.com>
2020-10-12 20:14:54 +0200

    working menu videobackgound and basic layout

---
+++ js/main.js
@@ -0,0 +1,17 @@
+const navButton = document.getElementById("navbarMenuButton");
+function menuTextSwap(){
+  if (navButton.innerHTML == "Menu") {
+    navButton.innerHTML = "Close";
+  }
+  else {
+    navButton.innerHTML = "Menu";
+  }
+  gsap.to(".navbar-toggler", { y: "-0%", duration: 1,});
+}
+
+const tl = gsap.timeline({ defaults: { ease: "power2.out" } });
+tl.set(".navbar", { y: "-300%", duration: 0,});
+tl.set(".header", { x: "-150%", duration: 0,});
+
+tl.to(".navbar", { y: "0%", duration: .6,});
+tl.to(".header", { x: "0%", duration: .5,});
