e5ed3d9781b916d02438c3de702e19318f620c4b
klovaaxel <klovakarlsson@gmail.com>
2020-10-13 10:26:32 +0200

    working landing slide, video background and responsive layout with lemon slice and text

--- js/main.js
+++ js/main.js
@@ -10,8 +10,8 @@ function menuTextSwap(){
 }
 
 const tl = gsap.timeline({ defaults: { ease: "power2.out" } });
-tl.set(".navbar", { y: "-300%", duration: 0,});
 tl.set(".header", { x: "-150%", duration: 0,});
+tl.set(".navbar", { y: "-300%", duration: 0,});
 
-tl.to(".navbar", { y: "0%", duration: .6,});
 tl.to(".header", { x: "0%", duration: .5,});
+tl.to(".navbar", { y: "0%", duration: .6,});
