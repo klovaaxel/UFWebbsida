4ca1c1c1294e2c07367f101fb21e63df33577ed7
klovaaxel <klovakarlsson@gmail.com>
2020-10-19 23:43:56 +0200

    contact page mobile and desktop layout

--- js/main.js
+++ js/main.js
@@ -10,8 +10,8 @@ function menuTextSwap(){
 }
 
 const tl = gsap.timeline({ defaults: { ease: "power2.out" } });
-tl.set(".header", { x: "-150%", duration: 0,});
-tl.set(".navbar", { y: "-300%", duration: 0,});
+tl.set(".header", { x: "-100vw", duration: 0,});
+tl.set(".navbar", { y: "-100vh", duration: 0,});
 
 tl.to(".header", { x: "0%", duration: .5,});
 tl.to(".navbar", { y: "0%", duration: .6,});
