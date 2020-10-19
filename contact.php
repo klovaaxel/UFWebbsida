ffcc86ce7007f4b73ce5cf42d6bce56656faf986
klovaaxel <klovakarlsson@gmail.com>
2020-10-19 17:49:39 +0200

    php and form

---
+++ contact.php
@@ -0,0 +1,19 @@
+<?php
+  $name = $_POST['name'];
+  $email = $_POST['email'];
+  $message = $_POST['message'];
+
+  $emal_from = 'ZITRUS UF'
+  $emal_subject = 'New Message From Zitrus Contact Form'
+  $emal_body =  "Name: $name.\n".
+                "Email: $email.\n".
+                "Message: $message.\n";
+
+  $to = "axel.karlsson01@skola.stenungsund.se";
+  $headers = "From: $email_from \r\n";
+  $headers .= "Reply-To: $email \r\n";
+
+  mail($to,$email_subject,$email_body,$headers);
+
+  header("location: index.html");
+?>
