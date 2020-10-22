3ce646c77d0406c4f2b638bd698958d92d2b4caf
klovaaxel <klovakarlsson@gmail.com>
2020-10-22 16:00:34 +0200

    more scaling fixes and lime in menu

--- js/main.js
+++ js/main.js
@@ -1,10 +1,14 @@
 const navButton = document.getElementById("navbarMenuButton");
+let navLime = document.getElementById("NavLime");
 function menuTextSwap(){
   if (navButton.innerHTML == "Menu") {
     navButton.innerHTML = "Close";
+    navLime.style.transform = "rotate(90deg) translateY(-.3em)"
   }
   else {
     navButton.innerHTML = "Menu";
+    navLime.style.transform = "rotate(0deg) translateY(-.3em)"
+
   }
   gsap.to(".navbar-toggler", { y: "-0%", duration: 1,});
 }
